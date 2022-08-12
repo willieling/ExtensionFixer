49088937fae7f1c7a3f831e16e576f309ed6c416
William Ling <44349092+willieling@users.noreply.github.com>
2022-08-12 05:49:33 -0400

    Initial commit

---
+++ FileAnalyzer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace ExtensionFixer
+{
+    public enum FileExtension
+    {
+        FileNotFound,
+        UnsupportedFormat,
+        gif,
+        webp,
+        webm,
+        mpg,
+        jpg,
+        png,
+        heic
+    }
+
+    internal static class FileAnalyzer
+    {
+        private const int EXTENSION_START_INDEX = 2;
+
+        public static FileExtension GetTrueFileExtension(string filepath)
+        {
+            string? extResult = Path.GetExtension(filepath);
+            if(string.IsNullOrEmpty(extResult))
+            {
+                return FileExtension.FileNotFound;
+            }
+
+            //remove dot prefix
+            extResult = extResult.Remove(0, 1);
+
+            foreach(FileExtension extension in Enum.GetValues(typeof(FileExtension)))
+            {
+                if(extension.ToString() == extResult)
+                {
+                    return extension;
+                }
+            }
+
+            return FileExtension.UnsupportedFormat;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExtensionFixer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenExplorerDialog_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+
+                PathTextBox.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void FixExtensionButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ScanFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
